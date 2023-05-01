d5f92054c24875ee2dee2dd7db0fe7a3a3bb2ee9
Tetiana-Kulyk <126335301+Tetiana-Kulyk@users.noreply.github.com>
2023-05-01 21:59:00 +0300

    Update driver_factory.py

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -1,6 +1,7 @@
 from selenium.webdriver import Chrome, Firefox
 from selenium.webdriver.chrome.service import Service as chrome_service
 from selenium.webdriver.firefox.service import Service as firefox_service
+from selenium.webdriver.chrome.options import Options
 
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
@@ -12,6 +13,9 @@ __SAFARI = 3
 
 def driver_factory(driver_id: int):
     if int(driver_id) == __CHROME:
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
         return Chrome(service=chrome_service(ChromeDriverManager().install()))
     elif int(driver_id) == __FIRE_FOX:
         return Firefox(service=firefox_service(GeckoDriverManager.install()))
