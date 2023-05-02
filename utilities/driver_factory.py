81b6157686a899c00553d8ab3697370381ff2c3d
Tetiana-Kulyk <126335301+Tetiana-Kulyk@users.noreply.github.com>
2023-05-03 00:20:38 +0300

    Update driver_factory.py

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -16,7 +16,7 @@ def driver_factory(driver_id: int):
         chrome_options = Options()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
-        return Chrome(service=chrome_service(ChromeDriverManager().install()))
+        return Chrome(service=chrome_service(ChromeDriverManager().install()), options=chrome_options)
     elif int(driver_id) == __FIRE_FOX:
         return Firefox(service=firefox_service(GeckoDriverManager.install()))
     elif int(driver_id) == __SAFARI:
