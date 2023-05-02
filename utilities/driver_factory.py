dc44370c9d8dee1fa784e563ab05c9bd09e873b9
Tetiana-Kulyk <126335301+Tetiana-Kulyk@users.noreply.github.com>
2023-05-02 23:16:44 +0300

    Update driver_factory.py

--- utilities/driver_factory.py
+++ utilities/driver_factory.py
@@ -14,9 +14,7 @@ __SAFARI = 3
 def driver_factory(driver_id: int):
     if int(driver_id) == __CHROME:
         chrome_options = Options()
-        chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-dev-shm-usage')
         return Chrome(service=chrome_service(ChromeDriverManager().install()), options=chrome_options)
     elif int(driver_id) == __FIRE_FOX:
         return Firefox(service=firefox_service(GeckoDriverManager.install()))
